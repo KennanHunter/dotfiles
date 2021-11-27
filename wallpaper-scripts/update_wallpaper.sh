f0e8cce9f68685440d832c6d004745909725482f
Kennan Hunter <kennanhunter5@gmail.com>
2021-11-27 03:01:18 -0500

    remove redundant image cache updates

--- wallpaper-scripts/update_wallpaper.sh
+++ wallpaper-scripts/update_wallpaper.sh
@@ -6,13 +6,13 @@ if [ -d "$1" ]; then
 	echo "setting wallpaper to $random"
 	notify-send -t 2000 "Changing Background" "$random"
 	wal -i "$random" --saturate 0.3
-	betterlockscreen -u "$random"
+	betterlockscreen -u "$random" --fx dim
 	exec /home/kennan/.config/wal/done.sh
 elif [ -f "$1" ]; then
 	echo "setting wallpaper to $1"
 	notify-send -t 2000 "Changing Background" "$1"
 	wal -i "$1" --saturate 0.3
-	betterlockscreen -u "$1"
+	betterlockscreen -u "$1" --fx dim
 	exec /home/kennan/.config/wal/done.sh
 else
 	echo "please provide either a file or directory"
