a9f4763bbba695e7eb84790ebd1c2335e616e50d
Kennan Hunter <kennanhunter5@gmail.com>
2022-01-24 02:14:51 -0500

    Update Wallpaper Script for Done notif

--- wallpaper-scripts/update_wallpaper.sh
+++ wallpaper-scripts/update_wallpaper.sh
@@ -7,13 +7,15 @@ if [ -d "$1" ]; then
 	notify-send -t 2000 "Changing Background" "$random"
 	wal -i "$random" --saturate 0.3
 	betterlockscreen -u "$random" --fx dim
-	exec /home/kennan/.config/wal/done.sh
+	/home/kennan/.config/wal/done.sh
+	exec notify-send -t 1000 "Changing Background" "Done"
 elif [ -f "$1" ]; then
 	echo "setting wallpaper to $1"
 	notify-send -t 2000 "Changing Background" "$1"
 	wal -i "$1" --saturate 0.3
 	betterlockscreen -u "$1" --fx dim
-	exec /home/kennan/.config/wal/done.sh
+	/home/kennan/.config/wal/done.sh
+	exec notify-send -t 1000 "Changing Background" "Done"
 else
 	echo "please provide either a file or directory"
 	notify-send -t 4000 "Changing Background" "Please provide a file or directory"
